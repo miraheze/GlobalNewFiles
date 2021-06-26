26a4c45a399a49c69ecd549c16cfca842fbdf1f2
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-06-26 14:52:20 -0600

    Update GlobalNewFilesPager.php

--- includes/GlobalNewFilesPager.php
+++ includes/GlobalNewFilesPager.php
@@ -4,7 +4,7 @@ use MediaWiki\MediaWikiServices;
 
 class GlobalNewFilesPager extends TablePager {
 	function __construct() {
-		$this->mDb = GlobalNewFilesHooks::getGlobalDB( DB_REPLICA );
+		$this->mDb = GlobalNewFilesHooks::getGlobalDB( DB_REPLICA, 'gnf_files' );
 
 		if ( $this->getRequest()->getText( 'sort', 'files_date' ) == 'files_date' ) {
 			$this->mDefaultDirection = IndexPager::DIR_DESCENDING;
