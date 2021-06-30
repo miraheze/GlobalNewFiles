7ce77929a066f95346ef6d717a40a1cd0ac95fe9
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-06-29 18:14:33 -0600

    Update GlobalNewFilesHooks.php

--- includes/GlobalNewFilesHooks.php
+++ includes/GlobalNewFilesHooks.php
@@ -8,49 +8,20 @@ class GlobalNewFilesHooks {
 	}
 
 	public static function onUploadComplete( $uploadBase ) {
-		$config = MediaWikiServices::getInstance()->getConfigFactory()->makeConfig( 'globalnewfiles' );
-
-		$uploadedFile = $uploadBase->getLocalFile();
-
-		$dbw = self::getGlobalDB( DB_PRIMARY );
-
-		$wiki = new RemoteWiki( $config->get( 'DBname' ) );
-
-		$dbw->insert(
-			'gnf_files',
-			[
-				'files_dbname' => $config->get( 'DBname' ),
-				'files_name' => $uploadedFile->getName(),
-				'files_page' => $config->get( 'Server' ) . $uploadedFile->getDescriptionUrl(),
-				'files_private' => (int)$wiki->isPrivate(),
-				'files_timestamp' => $dbw->timestamp(),
-				'files_url' => $uploadedFile->getViewURL(),
-				'files_user' => $uploadedFile->getUser()
-			],
-			__METHOD__
+		JobQueueGroup::singleton()->push(
+			new GlobalNewFilesInsertJob( [ 'uploadbase' => $uploadBase ] )
 		);
 	}
 
-	/**
-	 * Hook to FileDeleteComplete
-	 * @param File $file
-	 * @param File $oldimage
-	 * @param Article $article
-	 * @param User $user
-	 * @param string $reason
-	 */
 	public static function onFileDeleteComplete( $file, $oldimage, $article, $user, $reason ) {
-		$config = MediaWikiServices::getInstance()->getConfigFactory()->makeConfig( 'globalnewfiles' );
+		JobQueueGroup::singleton()->push(
+			new GlobalNewFilesDeleteJob( [ 'file' => $file ] )
+		);
+	}
 
-		$dbw = self::getGlobalDB( DB_PRIMARY );
-
-		$dbw->delete(
-			'gnf_files',
-			[
-				'files_dbname' => $config->get( 'DBname' ),
-				'files_name' => $file->getTitle()->getDBkey(),
-			],
-			__METHOD__
+	public static function onTitleMoveComplete( $title, $newTitle, $user, $oldid, $newid, $reason, $revision ) {
+		JobQueueGroup::singleton()->push(
+			new GlobalNewFilesMoveJob( [ 'title' => $title, 'newtitle' => $newTitle ] )
 		);
 	}
 
@@ -69,34 +40,6 @@ class GlobalNewFilesHooks {
 				__DIR__ . '/../sql/patch-gnf_files-binary.sql' 
 			);
 		}
-
-		return true;
-	}
-
-	public static function onTitleMoveComplete( $title, $newTitle, $user, $oldid, $newid, $reason, $revision ) {
-		$config = MediaWikiServices::getInstance()->getConfigFactory()->makeConfig( 'globalnewfiles' );
-
-		if ( !$title->inNamespace( NS_FILE ) ) {
-			return true;
-		}
-
-		$dbw = self::getGlobalDB( DB_PRIMARY );
-
-		$file = MediaWikiServices::getInstance()->getRepoGroup()->getLocalRepo()->newFile( $newTitle );
-
-		$dbw->update(
-			'gnf_files',
-			[
-				'files_name' => $file->getName(),
-				'files_url' => $file->getViewURL(),
-				'files_page' => $config->get( 'Server' ) . $file->getDescriptionUrl(),
-			],
-			[
-				'files_dbname' => $config->get( 'DBname' ),
-				'files_name' => $title->getDBKey(),
-			],
-			__METHOD__
-		);
 
 		return true;
 	}
