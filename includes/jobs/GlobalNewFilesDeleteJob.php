a184f10add44a5b7ef6df530bb1a815321102339
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-06-29 18:09:28 -0600

    Create GlobalNewFilesDeleteJob.php

---
+++ includes/jobs/GlobalNewFilesDeleteJob.php
@@ -0,0 +1,34 @@
+<?php
+
+use MediaWiki\MediaWikiServices;
+
+class GlobalNewFilesDeleteJob extends Job implements GenericParameterJob {
+	private $file;
+
+	/**
+	 * @param array $params
+	 */
+	public function __construct( array $params ) {
+		parent::__construct( 'GlobalNewFilesDeleteJob', $params );
+
+		$this->file = $params['file'];
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function run() {
+		$config = MediaWikiServices::getInstance()->getConfigFactory()->makeConfig( 'globalnewfiles' );
+
+		$dbw = self::getGlobalDB( DB_PRIMARY );
+
+		$dbw->delete(
+			'gnf_files',
+			[
+				'files_dbname' => $config->get( 'DBname' ),
+				'files_name' => $this->file->getTitle()->getDBkey(),
+			],
+			__METHOD__
+		);
+	}
+}
