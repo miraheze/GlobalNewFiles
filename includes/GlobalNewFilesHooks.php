d8f18e48e6b2af30461db92e43c158c02945c014
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-11-18 18:30:48 -0700

    Use addExtensionUpdateOnVirtualDomain

--- includes/GlobalNewFilesHooks.php
+++ includes/GlobalNewFilesHooks.php
@@ -66,28 +66,38 @@ class GlobalNewFilesHooks {
 	}
 
 	public static function onLoadExtensionSchemaUpdates( DatabaseUpdater $updater ) {
-		$updater->addExtensionTable(
+		$updater->addExtensionUpdateOnVirtualDomain( [
+			'virtual-globalnewfiles',
+			'addTable',
 			'gnf_files',
-			__DIR__ . '/../sql/gnf_files.sql'
-		);
+			__DIR__ . '/../sql/gnf_files.sql',
+			true,
+		] );
 
-		$updater->modifyExtensionField(
-			'gnf_files',
+		$updater->addExtensionUpdateOnVirtualDomain( [
+			'virtual-globalnewfiles',
+			'modifyField',
 			'files_timestamp',
-			__DIR__ . '/../sql/patches/patch-gnf_files-binary.sql'
-		);
+			__DIR__ . '/../sql/patches/patch-gnf_files-binary.sql',
+			true,
+		] );
 
-		$updater->addExtensionIndex(
-			'gnf_files',
+		$updater->addExtensionUpdateOnVirtualDomain( [
+			'virtual-globalnewfiles',
+			'addIndex',
 			'files_dbname',
-			__DIR__ . '/../sql/patches/patch-gnf_files-add-indexes.sql'
-		);
+			__DIR__ . '/../sql/patches/patch-gnf_files-add-indexes.sql',
+			true,
+		] );
 
-		$updater->addExtensionField(
+		$updater->addExtensionUpdateOnVirtualDomain( [
+			'virtual-globalnewfiles',
+			'addField',
 			'gnf_files',
 			'files_uploader',
-			__DIR__ . '/../sql/patches/patch-gnf_files-add-files_uploader.sql'
-		);
+			__DIR__ . '/../sql/patches/patch-gnf_files-add-files_uploader.sql',
+			true,
+		] );
 
 		$updater->addExtensionUpdateOnVirtualDomain( [
 			'virtual-globalnewfiles',
@@ -96,17 +106,23 @@ class GlobalNewFilesHooks {
 			PopulateUploaderCentralIds::class,
 		] );
 
-		$updater->modifyExtensionField(
+		$updater->addExtensionUpdateOnVirtualDomain( [
+			'virtual-globalnewfiles',
+			'modifyField',
 			'gnf_files',
 			'files_uploader',
-			__DIR__ . '/../sql/patches/patch-gnf_files-modify-files_uploader-default.sql'
-		);
+			__DIR__ . '/../sql/patches/patch-gnf_files-modify-files_uploader-default.sql',
+			true,
+		] );
 
-		$updater->dropExtensionField(
+		$updater->addExtensionUpdateOnVirtualDomain( [
+			'virtual-globalnewfiles',
+			'dropField',
 			'gnf_files',
 			'files_user',
-			__DIR__ . '/../sql/patches/patch-gnf_files-drop-files_user.sql'
-		);
+			__DIR__ . '/../sql/patches/patch-gnf_files-drop-files_user.sql',
+			true,
+		] );
 	}
 
 	/**
