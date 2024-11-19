c4c250f81e4f8afb73dbdb0120e7a83a9f6ab126
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-11-18 17:03:43 -0700

    Update phan config

--- .phan/config.php
+++ .phan/config.php
@@ -2,20 +2,9 @@
 
 $cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
 
-$cfg['directory_list'] = array_merge(
-	$cfg['directory_list'], [
-		'../../extensions/CreateWiki',
-	]
-);
-
-$cfg['exclude_analysis_directory_list'] = array_merge(
-	$cfg['exclude_analysis_directory_list'], [
-		'../../extensions/CreateWiki',
-	]
-);
-
 $cfg['suppress_issue_types'] = [
-	'SecurityCheck-LikelyFalsePositive'
+	'PhanAccessMethodInternal',
+	'SecurityCheck-LikelyFalsePositive',
 ];
 
 return $cfg;
