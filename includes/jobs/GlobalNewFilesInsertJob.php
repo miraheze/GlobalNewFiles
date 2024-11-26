0e23ca01a50bae32b09f5ecfe0e6b02f63adaf9c
Claire <blankeclair@disroot.org>
2024-11-26 15:00:02 +0000

    T12339: Add (untested) logging to hopefully catch it in prod (#75)
    * T12339: Add (untested) logging to hopefully catch it in prod
    
    * Set $ttl to null instead of a string
    
    * Check for lack of truthiness for $uploader
    
    > [27/11/2024 01:27] <MirahezeRelay> <paladox> The null check, wouldn’t it be 0?
    
    Honestly, It's 1:30 AM and I really can't be bothered to look at documentation
    for what it returns, so I'm just gonna do a generic falsiness test.

--- includes/jobs/GlobalNewFilesInsertJob.php
+++ includes/jobs/GlobalNewFilesInsertJob.php
@@ -1,5 +1,6 @@
 <?php
 
+use MediaWiki\Logger\LoggerFactory;
 use MediaWiki\MediaWikiServices;
 use MediaWiki\WikiMap\WikiMap;
 
@@ -24,8 +25,37 @@ class GlobalNewFilesInsertJob extends Job {
 
 		$dbw = GlobalNewFilesHooks::getGlobalDB( DB_PRIMARY );
 
-		$uploader = $uploadedFile->getUploader( File::RAW ) ??
-				$services->getActorStore()->getUnknownActor();
+		$uploader = $uploadedFile->getUploader( File::RAW );
+		if ( !$uploader ) {
+			$uploader = $services->getActorStore()->getUnknownActor();
+
+			// Slightly hacky logging in production for the elusive bug, T12339
+			$logger = LoggerFactory::getInstance( 'GlobalNewFiles' );
+			$logger->warning( 'GlobalNewFilesInsertJob: $uploader is null for {name}', [
+				'name' => $uploadedFile->getName(),
+			] );
+
+			try {
+				$cacheKey = $uploadedFile->getRepo()->getSharedCacheKey( 'file', sha1( $uploadedFile->getName() ) );
+				$logger->debug( 'GlobalNewFilesInsertJob: Cache key for {name}: {cacheKey}', [
+					'name' => $uploadedFile->getName(),
+					'cacheKey' => $cacheKey,
+				] );
+
+				$ttl = null;
+				$cachedData = $services->getMainWANObjectCache()->get( $cacheKey, $ttl );
+				$logger->debug( 'GlobalNewFilesInsertJob: Cached data for {name} (TTL: {ttl}): {cachedData}', [
+					'name' => $uploadedFile->getName(),
+					'ttl' => $ttl,
+					'cachedData' => $cachedData,
+				] );
+			} catch ( Exception $e ) {
+				$logger->warning( 'GlobalNewFilesInsertJob: Exception when grabbing internal MediaWiki details for {name}: {exception}', [
+					'name' => $uploadedFile->getName(),
+					'exception' => $e,
+				] );
+			}
+		}
 
 		$centralIdLookup = $services->getCentralIdLookup();
 
