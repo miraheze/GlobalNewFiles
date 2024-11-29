78267776a75fa6641692ae1d13a1214f023a029c
paladox <paladox@users.noreply.github.com>
2024-11-29 14:14:00 +0000

    gnf_files: Add index on files_private (#83)

--- sql/gnf_files.sql
+++ sql/gnf_files.sql
@@ -11,3 +11,4 @@ CREATE TABLE /*_*/gnf_files (
 CREATE INDEX /*i*/files_dbname ON /*_*/gnf_files (files_dbname);
 CREATE INDEX /*i*/files_timestamp ON /*_*/gnf_files (files_timestamp);
 CREATE INDEX /*i*/files_name ON /*_*/gnf_files (files_name);
+CREATE INDEX /*i*/files_private ON /*_*/gnf_files (files_private);
