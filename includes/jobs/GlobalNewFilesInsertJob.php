321e6ceea024bc36d3c02c17dc4433f8afccafb5
paladox <paladox@users.noreply.github.com>
2024-12-10 16:41:49 +0000

    Experiment

--- includes/jobs/GlobalNewFilesInsertJob.php
+++ includes/jobs/GlobalNewFilesInsertJob.php
@@ -1,5 +1,6 @@
 <?php
 
+use MediaWiki\Logger\LoggerFactory;
 use MediaWiki\MediaWikiServices;
 use MediaWiki\WikiMap\WikiMap;
 
@@ -26,6 +27,39 @@ class GlobalNewFilesInsertJob extends Job {
 		$permissionManager = $services->getPermissionManager();
 
 		$uploadedFile = $services->getRepoGroup()->getLocalRepo()->newFile( $this->getTitle() );
+
+		$uploadedFileTest = $services->getRepoGroup()->findFile( $this->getTitle(), [ 'ignoreRedirect' => true, 'latest' => true ] );
+		$uploader = $uploadedFileTest->getUploader( File::RAW );
+		if ( !$uploader ) {
+			// Slightly hacky logging in production for the elusive bug, T12339
+			$logger = LoggerFactory::getInstance( 'GlobalNewFiles' );
+			$logger->warning( 'GlobalNewFilesInsertJob: $uploader is null for {name}', [
+				'name' => $uploadedFileTest->getName(),
+				'uploader' => $uploader,
+				'fileTitle' => $this->getTitle(),
+				'uploadedFile' => $uploadedFileTest
+			] );
+			$uploader = $services->getActorStore()->getUnknownActor();
+			try {
+				$cacheKey = $uploadedFile->getRepo()->getSharedCacheKey( 'file', sha1( $uploadedFileTest->getName() ) );
+				$logger->debug( 'GlobalNewFilesInsertJob: Cache key for {name}: {cacheKey}', [
+					'name' => $uploadedFileTest->getName(),
+					'cacheKey' => $cacheKey,
+				] );
+				$ttl = null;
+				$cachedData = $services->getMainWANObjectCache()->get( $cacheKey, $ttl );
+				$logger->debug( 'GlobalNewFilesInsertJob: Cached data for {name} (TTL: {ttl}): {cachedData}', [
+					'name' => $uploadedFileTest->getName(),
+					'ttl' => $ttl,
+					'cachedData' => $cachedData,
+				] );
+			} catch ( Exception $e ) {
+				$logger->warning( 'GlobalNewFilesInsertJob: Exception when grabbing internal MediaWiki details for {name}: {exception}', [
+					'name' => $uploadedFileTest->getName(),
+					'exception' => $e,
+				] );
+			}
+		}
 
 		$dbw = GlobalNewFilesHooks::getGlobalDB( DB_PRIMARY );
 
